220672827ddca85ee3ff5b76d38aa4c0228161cf
maxym-valor <maxym.riabchuk@valor-software.com>
2021-09-29 13:53:36 +0300

    update iterations names

--- cypress/integration/wrong-way.js
+++ cypress/integration/wrong-way.js
@@ -1,5 +1,5 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
+describe('My third Test', () => {
+    it('should check wrong way 3', () => {
         cy.visit('http://localhost:3000/#/');
         cy.get('button[type="submit"]').click();
         cy.get('.ant-form-item:eq(0)').should('have.class', 'ant-form-item-has-error')
